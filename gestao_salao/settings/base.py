4160eebc4d3d0bdc03f374dd8fdd965bab92eab4
Leandro <le.chaves.silva@gmail.com>
2021-08-24 20:31:06 -0300

    correcao files colect static

--- gestao_salao/settings/base.py
+++ gestao_salao/settings/base.py
@@ -124,16 +124,19 @@ USE_L10N = False
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
-
-STATIC_URL = '/home/leandrochaves/Documentos/projetos/gestao_salao/static/'
+STATIC_URL = '/static/'
 
 # STATIC_ROOT = "/static/"
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
+
+# STATIC_URL = '/home/leandrochaves/Documentos/projetos/gestao_salao/static/'
 
 STATICFILES_DIRS = (
    '/home/leandrochaves/Documentos/projetos/gestao_salao/static/',
