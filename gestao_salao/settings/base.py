b152df621032f5b801cfffcef44e6542f6a3b4e9
Leandro <le.chaves.silva@gmail.com>
2021-09-01 21:05:50 -0300

    correcao

--- gestao_salao/settings/base.py
+++ gestao_salao/settings/base.py
@@ -125,7 +125,7 @@ USE_L10N = False
 USE_TZ = True
 
 
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
@@ -135,7 +135,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATIC_URL = '/home/leandrochaves/Documentos/projetos/gestao_salao/static/'
+# STATIC_URL = '/home/leandrochaves/Documentos/projetos/gestao_salao/static/'
 
 # STATICFILES_DIRS = (
 #    '/home/leandrochaves/Documentos/projetos/gestao_salao/static/',
