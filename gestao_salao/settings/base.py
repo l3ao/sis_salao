247c5f51341ee24fa7e8c7144eef6a2dabc32fc3
Leandro <le.chaves.silva@gmail.com>
2021-09-04 18:07:23 -0300

    commit

--- gestao_salao/settings/base.py
+++ gestao_salao/settings/base.py
@@ -124,13 +124,13 @@ USE_L10N = False
 
 USE_TZ = True
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
