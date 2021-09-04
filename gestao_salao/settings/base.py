63f88d6d0fc532bcda3450789bb69b1c6a34d4e7
Leandro <le.chaves.silva@gmail.com>
2021-09-04 17:29:28 -0300

    commit

--- gestao_salao/settings/base.py
+++ gestao_salao/settings/base.py
@@ -132,7 +132,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGIN_REDIRECT_URL = '/'
 
