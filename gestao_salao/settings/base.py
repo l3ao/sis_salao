c90c2ffddf50083a5857786e686aeee25ce86f27
Leandro <le.chaves.silva@gmail.com>
2021-09-04 18:01:14 -0300

    commit

--- gestao_salao/settings/base.py
+++ gestao_salao/settings/base.py
@@ -126,7 +126,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, 'static'),
