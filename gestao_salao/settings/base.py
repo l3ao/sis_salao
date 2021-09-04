5556a8c48156d7a6baaf21c12611c1025a4219ae
Leandro <le.chaves.silva@gmail.com>
2021-09-04 16:00:44 -0300

    commit

--- gestao_salao/settings/base.py
+++ gestao_salao/settings/base.py
@@ -132,7 +132,8 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 LOGIN_REDIRECT_URL = '/'
 
