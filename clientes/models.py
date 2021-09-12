5e46ffd989eeaa75bbd94d290e58e0a268377920
Leandro <le.chaves.silva@gmail.com>
2021-09-12 11:23:42 -0300

    commit

--- clientes/models.py
+++ clientes/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Cliente(models.Model):
     nome = models.CharField(max_length=100)
     data_nasc = models.DateField(blank=True, null=True)
-    telefone = models.CharField(max_length=11)
+    telefone = models.CharField(max_length=15)
     endereco = models.CharField(max_length=100)
 
     def __str__(self):
